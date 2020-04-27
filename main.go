f2755ad568fab9efe536e08696046c9ee340f448
Everton Macena <evertommp@gmail.com>
2020-04-27 11:32:50 -0300

    manager templates

--- main.go
+++ main.go
@@ -6,10 +6,18 @@ import (
 	"net/http"
 )
 
+type Post struct {
+	Id int
+	Title string
+	Body string
+}
+
 func main()  {
+	post := Post{Id: 1, Title: "First Post", Body: "ours content"}
+
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		t := template.Must(template.ParseFiles("templates/index.html"))
-		if err := t.ExecuteTemplate(writer, "index.html", nil); err != nil {
+		if err := t.ExecuteTemplate(writer, "index.html", post); err != nil {
 			http.Error(writer, err.Error(), http.StatusInternalServerError)
 		}
 	})
