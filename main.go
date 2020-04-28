13d58bc8204a26bd0a1fcbf35c40e7889a990af4
Everton Macena <evertommp@gmail.com>
2020-04-28 12:17:39 -0300

    database with go

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Post struct {
@@ -12,15 +14,47 @@ type Post struct {
 	Body string
 }
 
+var db, err = sql.Open("mysql", "root:root@tcp(192.168.99.100:3306)/go_db")
+
 func main()  {
-	post := Post{Id: 1, Title: "First Post", Body: "ours content"}
+	//stmt, err := db.Prepare("Insert into posts(title, body) values (?, ?)")
+	//checkErr(err)
+	//
+	//_, err = stmt.Exec("My first post", "My first content")
+	//checkErr(err)
+	//
+	rows, err := db.Query("Select * from posts")
+	checkErr(err)
+
+	items := []Post{}
+
+	for rows.Next() {
+		post := Post{}
+
+		rows.Scan(&post.Id, &post.Title, &post.Body)
+		items = append(items, post)
+	}
+
+	db.Close()
 
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		post := Post{Id: 1, Title: "Unamed Post", Body: "No content"}
+
+		if title := request.FormValue("title"); title != "" {
+			post.Title = title
+		}
+
 		t := template.Must(template.ParseFiles("templates/index.html"))
-		if err := t.ExecuteTemplate(writer, "index.html", post); err != nil {
+		if err := t.ExecuteTemplate(writer, "index.html", items); err != nil {
 			http.Error(writer, err.Error(), http.StatusInternalServerError)
 		}
 	})
 
 	fmt.Println(http.ListenAndServe(":3000", nil))
+}
+
+func checkErr(err error){
+	if err != nil {
+		panic(err.Error())  // Just for example purpose. You should use proper error handling instead of panic
+	}
 }
